30c4238829dac2e15a1e403e10c2ec4b69fb97e6
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2024-03-03 21:11:01 +0100

    Update memory every 5s

--- internal/controller/patch.go
+++ internal/controller/patch.go
@@ -5,9 +5,9 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -39,12 +39,12 @@ func (r *Reconciler) PatchResources(pod *corev1.Pod, namedResources NamedResourc
 	url := fmt.Sprintf("https://kubernetes.default.svc.cluster.local/api/v1/namespaces/%s/pods/%s", pod.Namespace, pod.Name)
 
 	for name, resources := range namedResources {
-		// Update with resources and not arbitrary 230Mi here
-		_ = resources
-
+		// TODO: update only when memory or cpu changes
+		newMemory := resources.memoryUsage * 2
 		body := []byte(fmt.Sprintf(
-			`{"spec": {"containers":[{"name":"%s", "resources":{"limits":{"memory": "230Mi", "cpu":"100m"},"requests":{"memory": "230Mi", "cpu":"100m"}}}]}}`,
-			name))
+			`{"spec": {"containers":[{"name":"%s", "resources":{"limits":{"memory": "%d", "cpu":"100m"},"requests":{"memory": "%d", "cpu":"100m"}}}]}}`,
+			name, newMemory, newMemory))
+
 		req, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(body))
 		if err != nil {
 			return ctrl.Result{}, err
@@ -52,19 +52,15 @@ func (r *Reconciler) PatchResources(pod *corev1.Pod, namedResources NamedResourc
 		req.Header.Add("Authorization", bearer)
 		req.Header.Add("Content-Type", "application/strategic-merge-patch+json")
 
+		// TODO: check that we receive 200 response
 		resp, err := client.Do(req)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
+		_ = resp
 
-		bodyBytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return ctrl.Result{}, err
-		}
-
-		r.Log.Info(string(bodyBytes))
-		r.Log.Info("successfuly patched container with new resources")
+		r.Log.Info(fmt.Sprintf("patched container with memory: %d and cpu: TODO", newMemory))
 	}
 
-	return reconcile.Result{}, nil
+	return reconcile.Result{RequeueAfter: 5 * time.Second}, nil
 }
