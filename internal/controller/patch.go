3cc6490c125c857854ece23f016162a5f6b0bbcd
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2024-03-06 11:00:46 +0100

    Divide PatchResources func

--- internal/controller/patch.go
+++ internal/controller/patch.go
@@ -30,26 +30,18 @@ const (
 	defaultCPUMax = 100
 )
 
+type ResourcesMinMax struct {
+	MemoryMin int64
+	MemoryMax int64
+
+	CPUMin float64
+	CPUMax float64
+}
+
 func (r *Reconciler) PatchResources(pod *corev1.Pod, namedResources NamedResources) (reconcile.Result, error) {
-	token, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
+	client, res, err := getK8SClient()
 	if err != nil {
-		return ctrl.Result{}, err
-	}
-	var bearer = "Bearer " + string(token)
-
-	caCert, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
-			},
-		},
+		return res, err
 	}
 
 	url := fmt.Sprintf("https://kubernetes.default.svc.cluster.local/api/v1/namespaces/%s/pods/%s", pod.Namespace, pod.Name)
@@ -71,43 +63,18 @@ func (r *Reconciler) PatchResources(pod *corev1.Pod, namedResources NamedResourc
 			cpuRatio = defaultCPURatio
 		}
 
-		memoryMinString, ok := pod.Annotations[fmt.Sprintf("unagex.com/%s-memory-min", name)]
-		memoryMinResource, err := resource.ParseQuantity(memoryMinString)
-		memoryMin := memoryMinResource.Value()
-		if !ok || err != nil || 0 > memoryMin {
-			memoryMin = defaultMemoryMin
-		}
-
-		memoryMaxString, ok := pod.Annotations[fmt.Sprintf("unagex.com/%s-memory-max", name)]
-		memoryMaxResource, err := resource.ParseQuantity(memoryMaxString)
-		memoryMax := memoryMaxResource.Value()
-		if !ok || err != nil || memoryMax < memoryMin {
-			memoryMax = defaultMemoryMax
-		}
-
-		cpuMinString, ok := pod.Annotations[fmt.Sprintf("unagex.com/%s-cpu-min", name)]
-		cpuMinResource, err := resource.ParseQuantity(cpuMinString)
-		cpuMin := cpuMinResource.AsApproximateFloat64()
-		if !ok || err != nil || 0 > cpuMin {
-			cpuMin = defaultCPUMin
-		}
-
-		cpuMaxString, ok := pod.Annotations[fmt.Sprintf("unagex.com/%s-cpu-max", name)]
-		cpuMaxResource, err := resource.ParseQuantity(cpuMaxString)
-		cpuMax := cpuMaxResource.AsApproximateFloat64()
-		if !ok || err != nil || cpuMax < cpuMin {
-			cpuMax = defaultCPUMax
-		}
+		// Get min and max memory and cpu.
+		resourcesMinMax := getResourcesMinMax(pod.Annotations, name)
 
 		// memory is in bytes
 		newMemory := int64(float64(resources.memoryUsage) * (1 / memoryRatio))
-		newMemory = max(memoryMin, newMemory)
-		newMemory = min(memoryMax, newMemory)
+		newMemory = max(resourcesMinMax.MemoryMin, newMemory)
+		newMemory = min(resourcesMinMax.MemoryMax, newMemory)
 
 		// cpu is in vCPU
 		newCPU := resources.cpuUsage * (1 / cpuRatio)
-		newCPU = max(cpuMin, newCPU)
-		newCPU = min(cpuMax, newCPU)
+		newCPU = max(resourcesMinMax.CPUMin, newCPU)
+		newCPU = min(resourcesMinMax.CPUMax, newCPU)
 
 		body := []byte(fmt.Sprintf(
 			`{"spec": {"containers":[{"name":"%s", "resources":{"limits":{"memory": "%d", "cpu":"%f"},"requests":{"memory": "%d", "cpu":"%f"}}}]}}`,
@@ -117,6 +84,11 @@ func (r *Reconciler) PatchResources(pod *corev1.Pod, namedResources NamedResourc
 		if err != nil {
 			return ctrl.Result{}, err
 		}
+		token, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		var bearer = "Bearer " + string(token)
 		req.Header.Add("Authorization", bearer)
 		req.Header.Add("Content-Type", "application/strategic-merge-patch+json")
 
@@ -131,4 +103,58 @@ func (r *Reconciler) PatchResources(pod *corev1.Pod, namedResources NamedResourc
 	}
 
 	return reconcile.Result{RequeueAfter: 5 * time.Second}, nil
+}
+
+func getK8SClient() (*http.Client, reconcile.Result, error) {
+	caCert, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+	if err != nil {
+		return nil, ctrl.Result{}, err
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	return &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: caCertPool,
+			},
+		},
+	}, reconcile.Result{}, nil
+}
+
+func getResourcesMinMax(annotations map[string]string, name string) ResourcesMinMax {
+	memoryMinString, ok := annotations[fmt.Sprintf("unagex.com/%s-memory-min", name)]
+	memoryMinResource, err := resource.ParseQuantity(memoryMinString)
+	memoryMin := memoryMinResource.Value()
+	if !ok || err != nil || 0 > memoryMin {
+		memoryMin = defaultMemoryMin
+	}
+
+	memoryMaxString, ok := annotations[fmt.Sprintf("unagex.com/%s-memory-max", name)]
+	memoryMaxResource, err := resource.ParseQuantity(memoryMaxString)
+	memoryMax := memoryMaxResource.Value()
+	if !ok || err != nil || memoryMax < memoryMin {
+		memoryMax = defaultMemoryMax
+	}
+
+	cpuMinString, ok := annotations[fmt.Sprintf("unagex.com/%s-cpu-min", name)]
+	cpuMinResource, err := resource.ParseQuantity(cpuMinString)
+	cpuMin := cpuMinResource.AsApproximateFloat64()
+	if !ok || err != nil || 0 > cpuMin {
+		cpuMin = defaultCPUMin
+	}
+
+	cpuMaxString, ok := annotations[fmt.Sprintf("unagex.com/%s-cpu-max", name)]
+	cpuMaxResource, err := resource.ParseQuantity(cpuMaxString)
+	cpuMax := cpuMaxResource.AsApproximateFloat64()
+	if !ok || err != nil || cpuMax < cpuMin {
+		cpuMax = defaultCPUMax
+	}
+	return ResourcesMinMax{
+		MemoryMin: memoryMin,
+		MemoryMax: memoryMax,
+
+		CPUMin: cpuMin,
+		CPUMax: cpuMax,
+	}
 }
