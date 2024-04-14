4cb5e3fa4baeb3e2d581125e935739be64e52f46
mathieu cesbron <mathieu.rudy.cesbron@gmail.com>
2024-04-14 16:01:43 +0000

    Add comments for cpu

--- pkg/controller/stats.go
+++ pkg/controller/stats.go
@@ -18,16 +18,13 @@ const (
 )
 
 const (
+	DefaultCPUMin       uint64  = 10
+	DefaultCPUMax       uint64  = 100_000
 	DefaultCPUInterval  uint64  = 6
 	DefaultCPUTargetAvg float64 = 0.8
 	DefaultCPUCoeff     uint64  = 6
 	DefaultCPUMaxInc    float64 = 0.5
 	DefaultCPUMaxDec    float64 = 0.1
-
-	// DefaultCPUMin in milliCPU, 10 is 0.01 cpu.
-	DefaultCPUMin uint64 = 10
-	// DefaultCPUMax in milliCPU, 100_000 is 100 cpus.
-	DefaultCPUMax uint64 = 100_000
 )
 
 type ContainerStats map[string]*Stats
@@ -59,22 +56,25 @@ type Memory struct {
 }
 
 type CPU struct {
+	// Limit is the cpu limit of the container in millicpus.
 	Limit int64
-	Min   uint64
-	Max   uint64
-
-	// TargetAvg is from 0 to 1. e.g. 0.8 means a target cpu usage of 80%.
+	// Min is the minimum cpu limit allowed on the container in millicpus.
+	Min uint64
+	// Max is the maximum cpu limit allowed on the container in millicpus.
+	Max uint64
+	// TargetAvg is the target cpu usage of the container. It is from 0 to 1. e.g. 0.8 means a target cpu usage of 80%.
 	TargetAvg float64
-
+	// MaxInc is the max cpu increase in percent allowed. For example 0.5 means kondense can increase the cpu limit up to 50%.
 	MaxInc float64
+	// MaxDec is the max cpu decrease in percent allowed. For example 0.5 means kondense can decrease the cpu limit up to 50%.
 	MaxDec float64
-	Coeff  uint64
-
+	// Coeff is used to calculate the new cpu limit when a cpu increase is needed. The higher the coeff, the higher the new cpu limit.
+	Coeff uint64
+	// Interval is the interval in seconds used to calculate the cpu average usage.
 	Interval uint64
-	// Usage is a queue of capacity Interval.
+	// Usage is a queue to store total cpu usage at a specific time.
 	Usage []CPUProbe
-
-	// Avg in mCPU
+	// Avg is the cpu average usage in millicpus.
 	Avg uint64
 }
 
